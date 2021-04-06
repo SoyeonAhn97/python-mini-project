f6c2c7e42fa2a0e2975b08a5b6ab5899f7da00a5
Youngking0914 <Ace_lephant@naver.com>
2021-04-06 16:05:50 +0900

    ver.0.1

---
+++ main.py
@@ -0,0 +1,14 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = 'https://ecampus.kangnam.ac.kr'
+
+response = requests.get(url)
+if response:
+    print("response is OK")
+    print("response status code : ", response.status_code)
+    html = response.text
+    soup = BeautifulSoup(html, 'html.parser')
+    print(soup)
+else:
+    print("X")
