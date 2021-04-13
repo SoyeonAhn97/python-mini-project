6414dd855fe536a23ec8dae1085a49a5b991450f
Youngking0914 <Ace_lephant@naver.com>
2021-04-13 16:16:51 +0900

    session ID값을 넘겨받았으나 로그인 페이지 redirection이 안됨

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import urllib
 import requests
 from bs4 import BeautifulSoup
 
@@ -11,24 +12,35 @@ LOGIN_HEADER = {
 }
 LOGIN_DATA = {
     'input-username': '201704049',
-    'input-password': '****'
+    'input-password': '*a34013401'
 }
 
 # Session 생성, with 구문 안에서 세션 유지, with 를 벗어나면 자동으로 세션 종료
 with requests.Session() as s:
-    # 로그인 시도
-    login_req = s.post(LOGIN_URL, data=LOGIN_DATA, allow_redirects=False)
-    # 로그인 결과 : 200이 나오면 request 날리기 성공!
-    print(login_req.status_code)
-    # 로그인 실패 시 : 200이 안나오면 request 날리기 실패
-    if login_req.status_code != 200:
+    # 로그인 데이터를 서버로 전송하고 응답을 response 에 저장 한다.
+    response = s.post(LOGIN_URL, data=LOGIN_DATA)
+    # 로그인 결과 : 200이 나오면 정상 응답
+    print(response.status_code)
+    # 로그인 실패 시 : 200이 안나오면 오류
+    if response.status_code != 200:
         print('request 실패')
 
-    # cookie = login_req.headers['Set-Cookie']
-    # print(cookie)
+    # 'Set-Cookie'로 MoodleSession 이라는 세션 ID 값이 넘어옴을 알 수 있다.
+    print("********response.headers*******")
+    print(response.headers)
+    # cookie로 세션을 로그인 상태를 관리하는 상태를 확인해보기 위한 코드입니다.
+    print("******s.cookies.get.dict()*******")
+    print(s.cookies.get_dict())
 
-    # 페이지 html 가져오기
-    url_html = s.get(URL)
+    cookie = response.headers['Set-Cookie']
+    print(cookie)
+
+    # 여기서부턴 로그인이 된 세션이 유지됩니다. session 의 header 에는 Cookie 의 MoodleSession ID가 들어갑니다.
+    response = s.get(URL)
+
+
+    print("********response.headers*******")
+    print(response.headers)
     # soup 만들기
-    soup = BeautifulSoup(url_html.text, 'html.parser')
-    print(soup)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    ##print(soup)
